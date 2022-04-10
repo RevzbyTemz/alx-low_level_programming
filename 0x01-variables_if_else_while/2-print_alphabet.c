5ce323c337016e896d083b5a1a9211626adb4801
RevzbyTemz <temiherself@gmail.com>
2022-04-10 12:34:26 +0100

    commit message for 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,17 +8,16 @@ int main(void)
 
 
 {
-char lower_case;
-char upper_case;
+char alphabet;
 
-for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
 putchar(upper_case);
 }
 
 for (lower_case = 'a'; lower_case <= 'z'; lower_case--)
 {
-putchar(lower_case);
+putchar(alphabet);
 }
 
 putchar("\n");
