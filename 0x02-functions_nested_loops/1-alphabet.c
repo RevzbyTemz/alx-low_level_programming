85a473634f01884e5ee72b22b4a295251750065f
RevzbyTemz <temiherself@gmail.com>
2022-04-12 14:30:35 +0100

    commit message for task 0-putchar.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
 /**
- *  * print_alphabet - print alphabet lowercase
- *   *
- *    * Return: Always 0.
- *     */
+ *print_alphabet - print alphabet lowercase
+ *
+ *Return: Always 0.
+ */
 
 void print_alphabet(void)
 {
