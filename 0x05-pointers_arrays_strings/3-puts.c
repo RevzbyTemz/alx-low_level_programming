0d0b05705572e0db37d9ab309071a1a54ee55fdc
RevzbyTemz <temiherself@gmail.com>
2022-04-20 12:24:39 +0100

    commit message for 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,20 +2,20 @@
 /**
  * _puts - prints a string
  * @str: A pointer to an int that will be changed
- * 
+ *
  * Return: void which means our answer is correct
  */
 
 void _puts(char *str)
 {
-	char *c;
-	int m;
+char *c;
+int m;
 
-	c = str;
+c = str;
 
-	for (m = 0; c[m]; m++)
-	{
-		_putchar (c[m]);
-	}
-	_putchar('\n');
+for (m = 0; c[m]; m++)
+{
+_putchar (c[m]);
+}
+_putchar('\n');
 }
