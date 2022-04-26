425e6f0578bb5098378de5c6dae18f8d46df6961
RevzbyTemz <temiherself@gmail.com>
2022-04-26 12:36:04 +0100

    commit message for task 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,16 +9,18 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 
-{
+
 unsigned int i;
 
 i = 0;
 while (i < n)  /*Delacring WHILE*/
 {
-*(dest + i) = *(src + i); /*add 1 position dest and src*
+*(dest + i) = *(src + i); /*add 1 position dest and src*/
 i++;
 
-										} /*END WHILE*/
+	
+} 
+/*END WHILE*/
 
 
 return (dest);
