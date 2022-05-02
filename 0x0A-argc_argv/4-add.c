57ebd965f3966b098b46549cfe1d772a313bafdd
RevzbyTemz <temiherself@gmail.com>
2022-05-02 22:30:54 +0100

    for 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,20 +11,17 @@
  */
 
 int check_num(char *str)
-
 {
 /*Declaring variables*/
 unsigned int count;
 
 count = 0;
 while (count < strlen(str)) /*count string*/
-
 {
 if (!isdigit(str[count])) /*check if str there are digit*/
 {
 return (0);
 }
-
 count++;
 }
 return (1);
@@ -39,9 +36,7 @@ return (1);
  */
 
 int main(int argc, char *argv[])
-
 {
-
 /*Declaring variables*/
 int count;
 int str_to_int;
@@ -51,19 +46,16 @@ count = 1;
 while (count < argc) /*Goes through the whole array*/
 {
 if(check_num(argv[count]))
-
 {
 str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
 sum += str_to_int;
 }
-
 /*Condition if one of the number contains symbols that are not digits*/
 else
 {
 printf("Error\n");
 return (1);
 }
-
 count++;
 }
 
