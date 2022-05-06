795a98f6677b3cac863b49a618becdc3f26d36fd
RevzbyTemz <temiherself@gmail.com>
2022-05-06 16:44:50 +0100

    commit message for 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -45,18 +45,20 @@ int sum = 0;
 count = 1;
 while (count < argc) /*Goes through the whole array*/
 {
-if(check_num(argv[count]))
+if (check_num(argv[count]))
 
 {
 str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
 sum += str_to_int;
 }
+
 /*Condition if one of the number contains symbols that are not digits*/
 else
 {
 printf("Error\n");
 return (1);
 }
+
 count++;
 }
 
